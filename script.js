f9ee510b295381b135c7af0bc94eb2de533e04ab
saminoorsyed <saminoorsyed@gmail.com>
2022-06-15 16:39:33 -0400

    Finish rock paper scissors game
    plays 5 rounds of rock paper scissors and alerts the user who won and
    by how much. If there is a tie, that round is not finished and the round
    starts over again

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function computerPlay(){
-    return choice = Math.floor(Math.random()*3);
+    return Math.floor(Math.random()*3);
 }
 
 function getPlayerChoice(){
@@ -8,17 +8,65 @@ function getPlayerChoice(){
 }
 
 function convertChoiceToInt() {
-    let playerChoice = getPlayerChoice();
+    const playerChoice = getPlayerChoice();
     if (playerChoice === "rock") {
-        let playerComp = 0; 
+        return 0; 
     } else if (playerChoice === "paper") {
-        let playerComp = 1;
-    } else if (playerChoice === "paper") {
-        let playerComp = 2;
+        return 1;
+    } else if (playerChoice === "scissors") {
+        return 2;
     } else {
         alert("you didn't type in a valid options, try again!")
         return convertChoiceToInt()
     }
-    return playerComp
 }
 
+function PlayRPC(){
+    const playerChoice = convertChoiceToInt()
+    const computerChoice = computerPlay()
+    console.log(computerChoice)
+    console.log(playerChoice)
+    if (playerChoice === 0 && computerChoice === 2){
+        alert("You won! Rock beats Scissors")
+        return 1
+    } else if (playerChoice === 2 && computerChoice === 1){
+        alert("You won! Scissors beats Paper")
+        return 1
+    }else if (playerChoice === 1 && computerChoice === 0){
+        alert("You won! Paper beats Rock")
+        return 1
+    }else if (playerChoice === 0 && computerChoice === 1){
+        alert("You lost! Rock beats Scissors")
+        return 0
+    }else if (playerChoice === 1 && computerChoice === 2){
+        alert("You lost! Rock beats Scissors")
+        return 0
+    }else if (playerChoice === 2 && computerChoice === 1){
+        alert("You lost! Rock beats Scissors")
+        return 0
+    }else if (playerChoice === computerChoice){
+        alert("It's a tie! play again!")
+        PlayRPC()
+    }
+}
+
+function game(){
+    let result
+    let playerTally = 0
+    let computerTally = 0
+    for (let i = 0; i <5; i++) {
+        result = PlayRPC()
+        if (result === 1) {
+            playerTally++    
+        }else{
+            computerTally++
+        }
+    }
+    if (playerTally> computerTally) {
+        alert(`You won ${playerTally} of the 5 rounds`)
+    }else {
+        alert(`The computer won ${computerTally} of the 5 rounds`)
+    }
+}
+
+game()
