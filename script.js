976f5659c53f9b465cc517bc21965e949debcd56
saminoorsyed <saminoorsyed@gmail.com>
2022-06-15 16:03:33 -0400

    Implement getPlayer choice and convertChoiceToInt
    These functions prompt the user for a choice and then convert that
    choice to an integer from 0 to 2. they should be able to handle upper
    and lowercase strings. If an incorrect value is added then the user is
    prompted again

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ function computerPlay(){
     return choice = Math.floor(Math.random()*3);
 }
 
+function getPlayerChoice(){
+    let playerInput = prompt("Choose Rock, Paper or Scissors!")
+    return playerInput.toLowerCase();
+}
+
+function convertChoiceToInt() {
+    let playerChoice = getPlayerChoice();
+    if (playerChoice === "rock") {
+        let playerComp = 0; 
+    } else if (playerChoice === "paper") {
+        let playerComp = 1;
+    } else if (playerChoice === "paper") {
+        let playerComp = 2;
+    } else {
+        alert("you didn't type in a valid options, try again!")
+        return convertChoiceToInt()
+    }
+    return playerComp
+}
+
